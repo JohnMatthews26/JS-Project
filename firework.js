fc2ab3fa5ff96ffaab5a4e00ee5f946fbd138b7a
John Matthews <johnmatthews2634@gmail.com>
2017-09-01 10:08:08 -0700

    hover box

--- firework.js
+++ firework.js
@@ -185,8 +185,25 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
   const recordingCanvas = new createjs.Stage("recordingCanvas");
-
-
+  // const hoverBox = new createjs.Shape().graphics.beginFill("black").drawRect(0, 0, 100, 100);
+  // recordingCanvas.enableMouseOver();
+  // hoverBox.on("mouseover", handleMouseOver);
+  // hoverBox.on("mouseout", handleMouseOut);
+  // recordingCanvas.addChild(hoverBox);
+  // recordingCanvas.setChildIndex(hoverBox, 0);
+  // recordingCanvas.update();
+  // function handleMouseOver() {
+  //   hoverBox.graphics.clear().beginFill("yellow").drawRect(0, 0, 100, 80);
+  //   recordingCanvas.addChild(hoverBox);
+  //   recordingCanvas.setChildIndex(hoverBox, 0);
+  //   recordingCanvas.update();
+  // }
+  // function handleMouseOut(){
+  //   hoverBox.graphics.clear().beginFill("black").drawRect(0 , 0, 100, 80);
+  //   recordingCanvas.addChild(hoverBox);
+  //   recordingCanvas.setChildIndex(hoverBox, 0);
+  //   recordingCanvas.update();
+  // }
 
 
   recordingLabel.x = 0;
